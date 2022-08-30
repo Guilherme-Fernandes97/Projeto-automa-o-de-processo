98768d04cf075e6dee036d599db66d7f56078118
Guilherme-Fernandes97 <guilherme_fernandes1997@hotmail.com>
2022-08-29 21:54:04 -0300

    teste 29-08-2022

---
+++ automacao_de_processo.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import os
+import pathlib
+
+emails = pd.read_excel(r'Bases de Dados\Emails.xlsx')
+lojas = pd.read_csv(r'Bases de Dados\Lojas.csv', encoding='latin1', sep=';')
+vendas = pd.read_excel(r'Bases de Dados\Vendas.xlsx')
+
+#incluir nome das lojas a planilha vendas
+vendas = vendas.merge(lojas, on='ID Loja')
+
+dicionario_lojas = {}
+for loja in lojas['Loja']:
+    dicionario_lojas[loja] = vendas.loc[vendas['Loja']==loja, :]
+
+dia_indicador = vendas['Data'].max()
+
+caminho_backup = pathlib.Path(r'Backup Arquivos Lojas')
+arquivos_pasta_backup = caminho_backup.iterdir()
+
+lista_nomes_backup = [arquivo.name for arquivo in arquivos_pasta_backup]
+
+for loja in dicionario_lojas:
+    if loja not in lista_nomes_backup:
+        nova_pasta = caminho_backup / loja
+        nova_pasta.mkdir()
+
+    nome_arquivo = f'{dia_indicador.month}_{dia_indicador.day}_{loja}.xlsx'
+    local_arquivo = caminho_backup / loja / nome_arquivo
+    dicionario_lojas[loja].to_excel(local_arquivo)
+
+for loja in dicionario_lojas:
+    vendas_loja = dicionario_lojas[loja]
+    vendas_loja_dia = vendas_loja.loc[vendas_loja['Data']==dia_indicador, :]
+
+    faturamento_ano = vendas_loja['Valor Final'].sum()
+    faturamento_dia = vendas_loja_dia['Valor Final'].sum()
+
+    print(faturamento_dia)
+
+
